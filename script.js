a24cab36b7fec898d793e2f54b2f2debb0b13b6d
“lukemccrae” <lukemccrae@gmail.com>
2016-11-14 21:10:16 -0700

    got basic css layout done

--- script.js
+++ script.js
@@ -11,10 +11,10 @@ function initMap(latPosition, longPosition) {
 
     map = new google.maps.Map(document.getElementById('map'), {
         center: {
-            lat: 00,
-            lng: 00
+            lat: 39.7576958,
+            lng: -105.00724629999999
         },
-        zoom: 1
+        zoom: 2
     });
 }
 
@@ -34,13 +34,14 @@ $(document).ready(function() {
         map.panTo(panPoint)
         map.setZoom(16);
         initialize();
+        console.log(latPosition, longPosition);
     })
 
     function initialize() {
         var pyrmont = new google.maps.LatLng(latPosition, longPosition);
         map = new google.maps.Map(document.getElementById('map'), {
             center: pyrmont,
-            zoom: 15
+            zoom: 16
         });
         var request = {
             location: pyrmont,
@@ -82,124 +83,29 @@ $(document).ready(function() {
     function guessMe() {
         var randomNumber = Math.floor(Math.random() * allPlaceName.length);
         placeGuess.push(allPlaceName[randomNumber])
-        $('#greeting').append("The place you're looking for is a " + placeType[randomNumber] + ".")
+        $('#type').append("The place you're looking for is a " + placeType[randomNumber] + ".")
         console.log(allPlaceName.length);
         console.log(placeGuess);
         for (var i = 0; i < placeGuess[0].length; i++) {
-            if (isLetter(placeGuess[0][i]) === false) {
-                dashes.push(placeGuess[0][i])
-            }
             if (placeGuess[0][i] === " ") {
                 dashes[i] = "&nbsp;";
+            } else if (isLetter(placeGuess[0][i]) === false) {
+                dashes.push(placeGuess[0][i])
             } else {
                 dashes[i] = " _ ";
             }
         }
-        console.log(dashes);
+
 
         function isLetter(c) {
             return c.toLowerCase() != c.toUpperCase();
         }
+        console.log(dashes);
         $('#guessForm').append(dashes)
     }
 
     function isLetter(c) {
         return c.toLowerCase() != c.toUpperCase();
     }
+
 });
-
-
-
-// this didnt work.
-// var request = {
-//     location: panPoint,
-//     radius: 500
-// }
-//
-// function callback(results, status) {
-//     if (status == google.maps.places.PlacesServiceStatus.OK) {
-//
-//     }
-// }
-//
-// service = new google.maps.places.PlacesService(map);
-// service.nearbySearch(request, callback);
-// console.log(place);
-
-
-
-
-
-
-// 39.7640021,-105.1352965
-
-//https://maps.googleapis.com/maps/api/geocode/json?latlng=39.7640021,-105.1352965&key=AIzaSyB6mjYhp5ca_RPpOdHu_Ul7E-YY6BYzmms
-
-
-// $(document).ready(function() {
-//   $.get('https://maps.googleapis.com/maps/api/geocode/json?latlng=39.757726399999996,-105.0072618&key=AIzaSyB6mjYhp5ca_RPpOdHu_Ul7E-YY6BYzmms')
-//   .done( function (data) {
-//   console.log(data);
-//   })
-//   .fail(function (error){
-//     console.log(error);
-//   })
-// })
-
-
-// https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places
-
-
-//https://maps.googleapis.com/maps/api/place/nearbysearch/output?parameters
-
-
-
-// YELP API information
-//Consumer Key	xH8sgYlA_Vxb6sb6cZnZzA
-// Consumer Secret	ryxLd8J4YXN1GGBdr_HnY4T-4aw
-// Token	hmL6a2vYFMdjrpm7iLXeAd7gFvY0KuyS
-// Token Secret
-
-// $(document).ready(function() {
-//   $.get('https://api.yelp.com/v2/search/?term=food&location=denver,co')
-//   .done( function (data) {
-//   console.log(data);
-//   })
-//   .fail(function (error){
-//     console.log(error);
-//   })
-// })
-
-
-// function RandomWord() {
-//     var requestStr = "https://api.yelp.com/v2/search/?term=restaurant&location=denver,co&sort=1&radius_filter=500";
-//
-//     $.ajax({
-//         type: "GET",
-//         url: requestStr,
-//         dataType: "jsonp",
-//         success: function(data){
-//           console.log(data);
-//           fail: (function (error){
-//               console.log(error);
-//             })
-//         }
-//     });
-// }
-//
-//
-//         }
-//     });
-// }
-//
-//
-//         }
-//     });
-// }
-//
-//
-//         }
-//     });
-// }
-//
-//
--- test.js
+++ test.js
@@ -4,18 +4,20 @@ var placeGuess = ["a."]
 for (var i = 0; i < placeGuess[0].length; i++) {
     if (isLetter(placeGuess[0][i]) === false) {
         dashes.push(placeGuess[0][i])
-    }
-    if (placeGuess[0][i] === " ") {
+    } else if (placeGuess[0][i] === " ") {
         dashes[i] = "&nbsp;";
     } else {
         dashes[i] = " _ ";
     }
 }
-console.log(dashes);
+
 
 function isLetter(c) {
     return c.toLowerCase() != c.toUpperCase();
 }
+
 console.log(isLetter(placeGuess[0][1]))
+
+console.log(dashes);
 dashes.push(placeGuess[0][1]);
 console.log(dashes);
